cdf08228f37dc8d3783f9fc47b91e73a76d6296c
fhfelipefh <felipehonorio55@gmail.com>
2021-09-30 18:02:38 -0300

    operators testes

--- 01/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,7 +0,0 @@
-
-7
-pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
-b
-2src/test/java/com/fhfelipefh/reactor/MonoTest.java,6/2/62ce75a0f51e81ad54a2557a6097c12f4d1531b7
-b
-2src/test/java/com/fhfelipefh/reactor/FluxTest.java,7/3/7350cdcaa6bdbd8b0a632ae03950485485cc7a1a
--- 01/src/test/java/com/fhfelipefh/reactor/OperatorsTest.java
+++ 01/src/test/java/com/fhfelipefh/reactor/OperatorsTest.java
@@ -1,10 +1,17 @@
 package com.fhfelipefh.reactor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 public class OperatorsTest {
@@ -83,5 +90,89 @@ public class OperatorsTest {
                 .verifyComplete();
     }
 
+    @Test
+    public void subscribeOnIO() throws Exception {
+        Mono<List<String>> list = Mono.fromCallable(() -> Files.readAllLines(Paths.get("./textfile.txt")))
+                .log()
+                .subscribeOn(Schedulers.boundedElastic());
+
+        list.subscribe(s -> log.info("{}", s));
+        Thread.sleep(3000);
+        StepVerifier.create(list)
+                .expectSubscription()
+                .thenConsumeWhile(l -> {
+                    Assertions.assertFalse(l.isEmpty());
+                    log.info("Size {}", l.size());
+                    return true;
+                }).verifyComplete();
+    }
+
+    @Test
+    public void switchIfEmptyOperator() {
+        Flux<Object> flux = emptyFlux()
+                .switchIfEmpty(Flux.just("No more empty"))
+                .log();
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext("No more empty")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void deferOperation() {
+        Mono<Long> defer = Mono.defer(() -> Mono.just(System.currentTimeMillis()));
+        defer.subscribe(s -> log.info("{}", s));
+
+        AtomicLong atomicLong = new AtomicLong(0);
+        defer.subscribe(atomicLong::set);
+        Assertions.assertTrue(atomicLong.get() > 0);
+
+    }
+
+    private Flux<Object> emptyFlux() {
+        return Flux.empty();
+    }
+
+    @Test
+    public void concatOperator() {
+        Flux<String> f1 = Flux.just("a", "b");
+        Flux<String> f2 = Flux.just("c", "d");
+        Flux<String> log = Flux.concat(f1, f2).log();
+        StepVerifier.create(log)
+                .expectSubscription()
+                .expectNext("a", "b", "c", "d")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void concatWithOperator() {
+        Flux<String> f1 = Flux.just("a", "b");
+        Flux<String> f2 = Flux.just("c", "d");
+
+        Flux<String> log = f1.concatWith(f2).log();
+
+        StepVerifier.create(log)
+                .expectSubscription()
+                .expectNext("a", "b", "c", "d")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void combineLatestOperator() {
+        Flux<String> f1 = Flux.just("a", "b");
+        Flux<String> f2 = Flux.just("c", "d");
+        Flux<String> log = Flux.combineLatest(f1, f2, (fx1, fx2) -> fx1.toUpperCase() + fx2.toUpperCase()).log();
+        StepVerifier.create(log)
+                .expectSubscription()
+                .expectNext("BC","BD")
+                .expectComplete()
+                .verify();
+    }
+
+    
 
 }

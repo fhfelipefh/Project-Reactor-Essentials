e29d437fac2c335c200a0b4dd93bce34d5318d6d
fhfelipefh <felipehonorio55@gmail.com>
2021-09-30 13:45:11 -0300

    +testes

--- 01/src/test/java/com/fhfelipefh/reactor/OperatorsTest.java
+++ 01/src/test/java/com/fhfelipefh/reactor/OperatorsTest.java
@@ -30,7 +30,6 @@ public class OperatorsTest {
     @Test
     public void publishOnSimple() {
         Flux<Integer> flux = Flux.range(1, 4)
-
                 .map(i -> {
                     log.info("Map 1 - on Thread: {}", i, Thread.currentThread().getName());
                     return i;
@@ -46,5 +45,43 @@ public class OperatorsTest {
                 .verifyComplete();
     }
 
-    
+    @Test
+    public void multipleSubscribeOn() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .publishOn(Schedulers.single())
+                .map(i -> {
+                    log.info("Map 1 - on Thread: {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 2 - on Thread: {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void publishAndSubscribeOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .publishOn(Schedulers.single())
+                .map(i -> {
+                    log.info("Map 1 - on Thread: {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 2 - on Thread: {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+
 }
